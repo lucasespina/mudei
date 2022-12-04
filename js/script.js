09dd9b2a1987d71a152ce873570f93c384a5eda6
Lucas Espina <lucaseqc@al.insper.edu.br>
2022-12-04 04:24:27 -0300

    login-check

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
+function login_check(input){
+    var login = document.getElementById("inpot_login").value;
+    var password = document.getElementById("inpot_passworld").value;
+      if (login === "" || password === "") {
+      alert("Você deve prencher todos os campos")
+      } else {
+        location.href="menu_inicial.html"
 
-})
+      };
+    };
+
+function botao_login() {
+
+    button = document.querySelector('button[name="login_botao"]')
+    button.addEventListener("click", login_check);
+};
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    botao_login()
+});
